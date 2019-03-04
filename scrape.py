38a76db0da6093b856cb966f101c6cdfba961677
nutmeg-5000 <z5208738@student.unsw.edu.au>
2019-03-04 16:39:45 +1100

    removed error handling in favor of state checks for pdfs/frames

--- scrape.py
+++ scrape.py
@@ -4,7 +4,8 @@ import re
 import getpass
 import tabula
 
-LOGIN_URL = "https://webcms3.cse.unsw.edu.au/login"
+BASE_URL = "https://webcms3.cse.unsw.edu.au"
+LOGIN_URL = BASE_URL + "/login"
 
 def main():
     session_requests = requests.session()
@@ -26,44 +27,43 @@ def main():
     result = session_requests.post(LOGIN_URL, data = payload, headers = dict(referer = LOGIN_URL))
 
     # get list of courses
-    dashboard_url = "https://webcms3.cse.unsw.edu.au/dashboard"
+    dashboard_url = BASE_URL + "/dashboard"
     result = session_requests.get(dashboard_url, headers = dict(referer = dashboard_url))
     doc = html.fromstring(result.content)
-    course = []
+    # course_url = {}
     print("Your courses:")
     nav = doc.xpath('.//ul[@class="nav navbar-nav"]')[0]
     for item in nav:
-        course.append(item.text_content())
+        # course_url[item.text_content()] = BASE_URL + item[0].get("href") + "/outline"
         print(item.text_content())
     print()
 
     while True:
+    # for c in course_url:
         # get course outline
         outline_url = input("Enter course outline URL: ")
+        # outline_url = course_url[c]
         result = session_requests.get(outline_url, headers = dict(referer = outline_url))
         doc = html.fromstring(result.content)
-        try:
-            # check if there's a frame in the course outline page
-            doc_frame = doc.xpath('.//iframe')[0]
-            outline_url = doc_frame.get("src")
+
+        # check if there's a frame in the course outline page
+        doc_frame = doc.xpath('.//iframe')
+        # check if there's a PDF in the page
+        pdf_frame = doc.xpath('.//object[@type="application/pdf"]')
+        if (doc_frame):
+            outline_url = doc_frame[0].get("src")
             # redirect to the frame URL
             result = session_requests.get(outline_url, headers = dict(referer = outline_url))
             doc = html.fromstring(result.content)
-        except IndexError:
-            try:
-                # check if there's a PDF in the page
-                pdf_frame = doc.xpath('.//object[@type="application/pdf"]')[0]
-                outline_url = "https://webcms3.cse.unsw.edu.au" + pdf_frame.get("data")
-                # redirect to the PDF URL
-                result = session_requests.get(outline_url, headers = dict(referer = outline_url))
-                # download the PDF
-                with open("outline.pdf", "wb") as f:
-                    f.write(result.content)
-                # use tabula to convert all tables into a CSV
-                pdf_csv = tabula.convert_into("outline.pdf", "outline.csv", output_format = "csv", pages = "all" multiple_tables = True)
-            except IndexError:
-                pass
-            pass
+        elif (pdf_frame):
+            outline_url = BASE_URL + pdf_frame[0].get("data")
+            # redirect to the PDF URL
+            result = session_requests.get(outline_url, headers = dict(referer = outline_url))
+            # download the PDF
+            with open("outline.pdf", "wb") as f:
+                f.write(result.content)
+            # use tabula to convert all tables into a CSV
+            pdf_csv = tabula.convert_into("outline.pdf", "outline.csv", output_format = "csv", pages = "all", multiple_tables = True)
         print()
 
         # parse course outline html for assignment dates
@@ -102,7 +102,7 @@ def main():
         break
 
     # log out
-    logout_url = "https://webcms3.cse.unsw.edu.au/logout"
+    logout_url = BASE_URL + "/logout"
     result = session_requests.get(logout_url, headers = dict(referer = logout_url))
     print("Logged out")
 
