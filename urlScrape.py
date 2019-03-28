738d030ec02a908caa4a067c233d97aa0696b378
FaizAther <faizather@protonmail.com>
2019-03-29 02:49:25 +1100

    scraper works

--- urlScrape.py
+++ urlScrape.py
@@ -23,22 +23,18 @@ def Urlopen(url):
         print('The server could not be found')
         return None
     else:
-        print('It worked')
+        #print('It worked')
         return html
 
 # Follow Meta Refrest Tag
 def followRefresh(url):
     try:
-        #print(url)
         response = Urlopen(url)
         html = soup(response.read(), 'html.parser')
-        #print(response.read())
         print(html.geturl())
         element = html.find('meta', attrs={'http-equiv': 'refresh'})
-        #print(element)
         refreshContent = element['content']
         refUrl = refreshContent.partition('=')[2]
-        #print(url)
         response.close()
         return refUrl
     except:
@@ -53,49 +49,37 @@ def cleanString(str):
             pass
         else:
             newstr+=str[i]
-
     return newstr
 
 
 # Generate forward link
 def genLink(base, forward):
-
     forward = cleanString(forward)
-    #print('->', forward)
     ba = re.split('/', base)
     fo = re.split('/', forward)
-    #print('->', fo)
     if BASE_URL_2 in forward:
         return forward
     elif BASE_URL in forward:
         return forward
     elif 'http' in forward:
         return forward
-    #i = 0
     for f in fo:
         found = False
         for b in ba:
             if f == b:
                 found = True
         if found == False:
-            #print('->', base, f)
             if f[0] != '/' and base[-1] == '/':
                 base = base + (f)
-
-            #print('->', f)
             elif base[-1] == '/' and f[0] == '/':
                 f = f[1:]
-                #print('->', f)
                 base = base + f
             else:
                 base = base + '/' + (f)
-        #i+=1
-
     return base
 
-
+courseCode = input("enter course code e.g. 1511: ")
 #html = Urlopen('http://cgi.cse.unsw.edu.au/~' + subjectCode + courseCode + '/' + )
-#subjectCode1 = input("enter course code e.g. COMP1511: ")
 searchTerm = input("enter search term e.g. outline: ")
 try:
     refUrl = followRefresh(BASE_URL + '/' + subjectCode1 + courseCode + '/' + year)
@@ -103,17 +87,9 @@ try:
     bs = soup(html.read(), 'lxml')
 except:
     refUrl = followRefresh(BASE_URL_2 + '/' + '~' + subjectCode2 + courseCode + '/')
-#print(refUrl)
     html = Urlopen(refUrl)
-#html = Urlopen('https://www.google.com')
     bs = soup(html.read(), 'lxml')
-#print(bs.body)
 
 for ass in bs.find_all('a'):
-    #print(ass['href'].partition('/'))
-
     if searchTerm in str(ass).lower():
-        #print(ass['href'])
         print(genLink(refUrl, ass['href']))
-    #print(genLink(refUrl, ass['href']))
-#print(bs.h1)
